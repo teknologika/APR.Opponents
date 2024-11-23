30f299b3bd479d2eab529927f2d52eb5951ed008
Bruce McLeod <bruce@teknologika.com>
2024-11-23 11:19:09 +1100

    Added release target to fix WPF designer rendering issues

---
+++ APRiRacingSettings.cs
@@ -0,0 +1,15 @@
+﻿using GameReaderCommon.Replays;
+
+namespace APR.SimhubPlugins
+{
+    /// <summary>
+    /// Settings class, make sure it can be correctly serialized using JSON.net
+    /// </summary>
+    public class APRiRacingSettings
+    {
+        public double LowFuelWarningLevel = 5.0;
+        public int MAX_CARS = 64;
+        public bool OverrideJavaScriptFunctions = true;
+        public bool RelativeShowCarsInPits = false;
+    }
+}
---
+++ SettingsControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using SimHub.Plugins.Styles;
+using System.Windows.Controls;
+
+namespace APR.SimhubPlugins {
+    /// <summary>
+    /// Interaction logic for APRiRacingSettingsControl.xaml
+    /// </summary>
+    public partial class APRiRacingSettingsControl : UserControl
+    {
+        public APRiRacingPlugin Plugin { get; }
+
+        public APRiRacingSettingsControl()
+        {
+            InitializeComponent();
+        }
+
+        public APRiRacingSettingsControl(APRiRacingPlugin plugin) : this()
+        {
+            this.Plugin = plugin;
+        }
+
+        private async void StyledMessageBox_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var res = await SHMessageBox.Show("Message box", "Hello", System.Windows.MessageBoxButton.OKCancel, System.Windows.MessageBoxImage.Question);
+
+            await SHMessageBox.Show(res.ToString());
+        }
+
+        private void DemoWindow_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var window = new  DemoWindow();
+
+            window.Show();
+        }
+
+        private async void DemodialogWindow_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var dialogWindow = new DemoDialogWindow();
+
+            var res = await dialogWindow.ShowDialogWindowAsync(this);
+
+            await SHMessageBox.Show(res.ToString());
+        }
+    }
+}
